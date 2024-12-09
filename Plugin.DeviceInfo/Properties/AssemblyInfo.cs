1ff97f58422c245fb1b2a3f2e47fd13978478dff
DKorablin2 <danila.korablin@unity3d.com>
2024-12-09 23:05:29 +0300

    Updated SAL.Flatbed 1.2.10

--- Plugin.DeviceInfo/Properties/AssemblyInfo.cs
+++ Plugin.DeviceInfo/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.InteropServices;
 #endif
 [assembly: AssemblyCompany("Danila Korablin")]
 [assembly: AssemblyProduct("Plugin.DeviceInfo")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2013")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2013-2024")]
 #endif
 
 /*if $(ConfigurationName) == Release (
